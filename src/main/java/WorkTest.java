9c2be9ab33208183819d72bf74b20e9f69054f93
ShadowOfFallenLord <ShadowOfFallenLord@users.noreply.github.com>
2020-09-30 23:37:30 +0400

    Init commit and work test

---
+++ src/main/java/WorkTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WorkTest
+{
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver",
+                ".\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        WebElement search_string = driver.findElement(By.cssSelector(".gLFyf"));
+        search_string.sendKeys("How to test web pages with Selenium?");
+        search_string.submit();
+    }
+}
